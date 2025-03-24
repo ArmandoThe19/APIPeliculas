ad5b8e25807b5c2037410cc66cce6ca8bb1a2130
Sandoval <Sandoval@DESKTOP-JIGF90A>
2025-03-24 00:50:36 -0500

    Agregar archivos de proyecto.

---
+++ Peliculas.cs
@@ -0,0 +1,7 @@
+﻿public class Pelicula
+{
+    public required string Id { get; set; }
+    public required string Titulo { get; set; }
+    public required string Sinopsis { get; set; }
+    public required string Link { get; set; }
+}
